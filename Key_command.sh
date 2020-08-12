f700c7476747ccc2e9bdf901663c816f4152cd0e
dieunelderilus <dieunelderilus@gmail.com>
2020-08-12 11:37:45 -0400

    Create Key_command.sh

---
+++ Key_command.sh
@@ -0,0 +1,10 @@
+##  Downloading *.sra files  from ncbi using accession number with 'wget
+
+get https://ftp-trace.ncbi.nih.gov/sra/sra-instant/reads/ByRun/sra/ERR/ERR315/ERR315856/ERR315856.sra
+wget https://ftp-trace.ncbi.nih.gov/sra/sra-instant/reads/ByRun/sra/ERR/ERR315/ERR315857/ERR315857.sra
+wget https://ftp-trace.ncbi.nih.gov/sra/sra-instant/reads/ByRun/sra/ERR/ERR315/ERR315858/ERR315858.sra
+wget https://ftp-trace.ncbi.nih.gov/sra/sra-instant/reads/ByRun/sra/ERR/ERR315/ERR315859/ERR315859.sra
+wget https://ftp-trace.ncbi.nih.gov/sra/sra-instant/reads/ByRun/sra/ERR/ERR315/ERR315862/ERR315862.sra
+wget https://ftp-trace.ncbi.nih.gov/sra/sra-instant/reads/ByRun/sra/ERR/ERR318/ERR318618/ERR318618.sra
+wget https://ftp-trace.ncbi.nih.gov/sra/sra-instant/reads/ByRun/sra/ERR/ERR588/ERR588857/ERR588857.sra
+wget https://ftp-trace.ncbi.nih.gov/sra/sra-instant/reads/ByRun/sra/ERR/ERR594/ERR594284/ERR594284.sra
